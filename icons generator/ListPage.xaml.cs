ec3e37ea1c91d41ba1f018ec63debb6e05815087
Краснов Роберт <krasnovrobert.9@gmail.com>
2024-02-01 01:13:49 +0300

    Добавлены пояснительны комментарии

--- icons generator/ListPage.xaml.cs
+++ icons generator/ListPage.xaml.cs
@@ -3,17 +3,33 @@ using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
 using System.Text.Json;
 using System.Xml;
-using Windows.Security.Cryptography.Certificates;
 
 namespace icons_generator;
 
 public partial class ListPage : ContentPage
 {
+    /// <summary>
+    /// Путь к файлу json с размерами изображений для платформ
+    /// </summary>
     String path = String.Empty;
+
+    /// <summary>
+    /// Объект в который помещены данные из json файла по пути @path
+    /// </summary>
     PlatformType platformTypeInFile;
+
+    /// <summary>
+    /// Объект для вывода размеров изображений в ListView на странице приложения
+    /// </summary>
     public ObservableCollection<ImageSizes> ImageSizes { get; set; } = new ObservableCollection<ImageSizes>();
 
+    /// <summary>
+    /// Список размеров изображений для Android
+    /// </summary>
     List<string> sizesAndroid = new List<string>();
+    /// <summary>
+    /// Список размеров изображений для iOS
+    /// </summary>
     List<string> sizesIOS = new List<string>();
     public ListPage()
     {
@@ -22,35 +38,59 @@ public partial class ListPage : ContentPage
     }
 
     private async void ConfirmClicked(object sender, EventArgs e)
-    { }
+    { 
+
+    }
     private async void SizesListItemTapped(object sender, ItemTappedEventArgs e)
     {
 
     }
+    /// <summary>
+    /// Метод обрабатывает нажатие на кнопку AddButton(добавить)
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private async void ListItemAddClicked(object sender, EventArgs e)
     {
+        // Переменная newSize хранит в себе данные введеные в popUp
         var newSize = await DisplayPromptAsync("Добавление размера", "Введите размер в формате(0х0):", "OK", "Отмена");
         if (newSize != null)
         {
+            // Обновляем объект размеров изображения, добавляем туда новое значение
+            List<string> sizes = platformTypeInFile.Android.Sizes.ToList();     ///
+            sizes.Add(newSize);                                                 ///
+            platformTypeInFile.Android.Sizes = sizes.ToArray();                 ///
+            ///////////////////////////////////////////////////////////////////////
+
+            // Добавляем в список размеров изображения новое значение
             sizesAndroid.Add(newSize);
-            List<string> sizes = platformTypeInFile.Android.Sizes.ToList();
-            sizes.Add(newSize);
-            platformTypeInFile.Android.Sizes = sizes.ToArray();
+           
+            // Обновляем ListView которая находится на странице
             UpdateList();
         }
     }
 
+    /// <summary>
+    /// Метод обрабатывает нажатие на кнопку DeleteButton(удалить)
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private async void ListItemDelClicked(object sender, EventArgs e)
     {
         if (SizesList.SelectedItem != null)
         {
             try
             {
-                var delItem = sizesAndroid.First(p => p == SizesList.SelectedItem);
-                sizesAndroid.Remove(delItem);
-                List<string> sizes = platformTypeInFile.Android.Sizes.ToList();
-                sizes.Remove(delItem);
-                platformTypeInFile.Android.Sizes = sizes.ToArray();
+                // Находим выбранный элемент списка(UI) и удаляем ее из списка(переменная)  //
+                var delItem = sizesAndroid.First(p => p == SizesList.SelectedItem);        //
+                sizesAndroid.Remove(delItem);                                             //
+                ///////////////////////////////////////////////////////////////////////////
+
+                // Обновляем объект размеров изображения, удаляем выбранный элемент списка //
+                List<string> sizes = platformTypeInFile.Android.Sizes.ToList();           //
+                sizes.Remove(delItem);                                                   //
+                platformTypeInFile.Android.Sizes = sizes.ToArray();                     //
+                /////////////////////////////////////////////////////////////////////////
             }
             catch
             {
@@ -77,6 +117,9 @@ public partial class ListPage : ContentPage
         }
     }
 
+    /// <summary>
+    /// Функция обновления ListView
+    /// </summary>
     private void UpdateList()
     {
         var sizes = new ImageSizesViewModel(sizesAndroid);
@@ -84,6 +127,18 @@ public partial class ListPage : ContentPage
         ReSaveJson();
     }
 
+    /// <summary>
+    /// Функция обновления ListView(перегрузка)
+    /// </summary>
+    private void UpdateList(bool needReSave)
+    {
+        var sizes = new ImageSizesViewModel(sizesAndroid);
+        SizesList.BindingContext = sizes;
+    }
+
+    /// <summary>
+    /// Пересохранение файла json, который содержит в себе размеры изображения для платформ
+    /// </summary>
     private void ReSaveJson()
     {
         if (File.Exists(path))
