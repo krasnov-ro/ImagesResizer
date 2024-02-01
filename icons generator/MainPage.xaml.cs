c21ef235399f9c6caa3c20224f48a86dcf4c08e4
Краснов Роберт <krasnovrobert.9@gmail.com>
2024-02-01 03:17:12 +0300

    Добавлена возможность переоткрытия изображения

--- icons generator/MainPage.xaml.cs
+++ icons generator/MainPage.xaml.cs
@@ -5,36 +5,34 @@ namespace icons_generator;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        CounterBtn.Clicked += PickPhoto_Clicked;
+    }
 
     private async void PickPhoto_Clicked(object sender, EventArgs e)
     {
-        if (CounterBtn.Text != "Настройка размеров")
+        FileResult result = await FilePicker.PickAsync(new PickOptions
         {
-            FileResult result = await FilePicker.PickAsync(new PickOptions
-            {
-                PickerTitle = "Pick a Photo",
-                FileTypes = FilePickerFileType.Images
-            });
+            PickerTitle = "Pick a Photo",
+            FileTypes = FilePickerFileType.Images
+        });
 
-            if (result == null) return;
+        if (result == null) return;
+        FileName.Text = result.FileName;
+        MainImage.Source = result.FullPath;
 
-            FileName.Text = result.FileName;
-            MainImage.Source = result.FullPath;
-            CounterBtn.Text = "Настройка размеров";
-        }
-        else
-        {
-            ToListPage(sender, e);
-        }
+        if (ReOpenBtn.IsVisible == true) return;
+        CounterBtn.Text = "Настройка размеров";
+        CounterBtn.Clicked -= PickPhoto_Clicked;
+        CounterBtn.Clicked += ToListPage;
+        ReOpenBtn.IsVisible = true;
     }
 
-    private async void ToListPage(object? sender, EventArgs e)
+    private async void ToListPage(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new ListPage());
     }
